34f09137cc34aa76c33b7afd37d7e49b522c7b63
xpendence <slava_rossii@list.ru>
2017-12-16 13:07:54 +0300

    in process

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package repository;
 
+import entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
